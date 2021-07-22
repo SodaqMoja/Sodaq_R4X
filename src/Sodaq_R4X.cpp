dd76efc999bc7f33ecf95ad36d65dd6f4b981550
Jan van Loenen <git@janvanloenen.nl>
2021-07-22 11:02:44 +0200

    Connect to socket if socket is closed

--- src/Sodaq_R4X.cpp
+++ src/Sodaq_R4X.cpp
@@ -1104,6 +1104,10 @@ size_t Sodaq_R4X::socketSend(uint8_t socketID, const char* remoteHost, const uin
         return 0;
     }
 
+    if (_socketClosed[socketID]) {
+        socketConnect(socketID, remoteHost, remotePort);
+    }
+
     /* Show the socket error
      * Just is just for diagnostics. Somehow we always see error 65
      * after doing a USOST. 65 is EEOF - End of file.
